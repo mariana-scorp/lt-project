cd2e7c631b207b77b845caf31abb53d93534e026
Mariana <mariana.scorp@gmail.com>
2014-10-30 09:35:52 +0200

    filtered wordlists added

---
+++ our_dictionary_additions/files_from_Natalia/filter.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import os
+
+file_list = []
+
+for f in os.listdir("."):
+    if f.endswith(".txt"):
+        file_list.append(f)
+
+for i in file_list:
+    f = open(i, "r")
+    f1 = open(i[:-4] + "_filtered.txt", "w")
+    wordlist = []
+    for line in f.readlines():
+        if (u"\ ".encode("utf-8") not in line):
+            wordlist.append(line)
+
+    wordlist = set(wordlist)
+    wordlist = sorted(wordlist)
+    for i in wordlist:
+        f1.write(i)
+
+    f.close()
+    f1.close()
