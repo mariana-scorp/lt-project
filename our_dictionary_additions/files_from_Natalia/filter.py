f3765f3cc898594bfd45fedd2f8f272d7da98a63
Mariana <mariana.scorp@gmail.com>
2014-11-02 15:40:20 +0200

    apostrophe bug fixed

--- our_dictionary_additions/files_from_Natalia/filter.py
+++ our_dictionary_additions/files_from_Natalia/filter.py
@@ -4,7 +4,7 @@ import os
 file_list = []
 
 for f in os.listdir("."):
-    if (f.endswith(".txt") and not f.endswith("_filtered.txt")):
+    if (f.endswith(".txt") and not f.endswith("_filtered.txt") and not f.endswith("_new.txt")):
         file_list.append(f)
 
 for i in file_list:
@@ -13,7 +13,12 @@ for i in file_list:
     wordlist = []
     for line in f.readlines():
         if (u"\ ".encode("utf-8") not in line):
-            wordlist.append(line)
+            apostrophe = line.find("\"")
+            if (apostrophe != -1):
+                word = line[:apostrophe] + "'" + line[apostrophe+1:]
+                wordlist.append(word)
+            else:
+                wordlist.append(line)
 
     wordlist = set(wordlist)
     wordlist = sorted(wordlist)
